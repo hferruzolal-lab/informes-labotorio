21e05709068bff4dd019d39c9b154e4cdf72b0c0
Samuel <hferruzolal.unemi.edu.ecm>
2026-07-06 15:01:53 -0500

    Actualizar configuracion de correo

--- config/settings.py
+++ config/settings.py
@@ -141,19 +141,11 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get(
-    "EMAIL_HOST_USER",
-    "samuelferruzola02@gmail.com"
-)
-
-EMAIL_HOST_PASSWORD = os.environ.get(
-    "EMAIL_HOST_PASSWORD",
-    "hpbhxhppdnajnlik"
-)
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = EMAIL_HOST_USER
-
 
 # Default primary key field type
 
