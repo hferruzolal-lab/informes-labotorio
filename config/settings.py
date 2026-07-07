7803f3c696177611aeafd73506de17d571ce1524
Samuel <hferruzolal.unemi.edu.ecm>
2026-07-07 01:49:14 -0500

    Agregar timeout al correo

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 20
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
